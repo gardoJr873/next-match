3aa2f8f726a99ca77625a278cb5beada9c6c5689
edgardo.ortiz.jr11 <edgardo.ortiz.jr11@gmail.com>
2025-06-29 10:59:21 -0500

    new login features

--- src/app/(auth)/login/LoginForm.tsx
+++ src/app/(auth)/login/LoginForm.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
-import {Card, CardHeader} from "@heroui/card";
+import {Card, CardBody, CardHeader} from "@heroui/card";
+import {GiPadlock} from "react-icons/gi";
+import {Input} from "@heroui/input";
 
 
 function LoginForm() {
   return (
     <Card className={'w-2/5 mx-auto'}>
       <CardHeader className={'flex flex-col items-center justify-center'}>
-
+        <div className={'flex flex-col gap-2 items-center text-secondary'}>
+          <div className={'flex flex-row items-center gap-3'}>
+            <GiPadlock size={30}/>
+            <h1 className={'text-3xl font-semibold'}>Login</h1>
+          </div>
+          <p className={'text-neutral-500'}>Welcome back to NextMatch</p>
+        </div>
       </CardHeader>
+      <CardBody>
+        <form action="">
+          <div>
+            <Input>
+
+            </Input>
+          </div>
+        </form>
+      </CardBody>
     </Card>
   );
 }
